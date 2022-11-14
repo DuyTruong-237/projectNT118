c72a84cbeb9e0b14600f653c2ff1be46142062aa
DuyTruong-080 <113657419+DuyTruong-237@users.noreply.github.com>
2022-11-14 13:04:53 +0700

    update #1
    - login activity

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.annotation.DrawableRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        moveActivity();
+    }
+
+    private void moveActivity() {
+        CountDownTimer countDownTimert=new CountDownTimer(2000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Intent intent = new Intent(MainActivity.this,activity_login.class );
+                startActivity(intent);
+            }
+        };
+        countDownTimert.start();
+    }
+}
